bbf7fd814869e6eb99b28b1cc1277bdeaa4d9407
Ev Ooi <hello@evthedev.com>
2017-09-21 23:42:49 +0800

    First commit

---
+++ src/components/Hello.test.ts
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Hello } from './Hello'
+import * as TestUtils from 'react-addons-test-utils'
+
+
+test('It should render Hello component', () => {
+
+})
---
+++ src/components/Hello.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+export interface Props {
+    compiler: string
+    framework: string
+}
+
+export class Hello extends React.Component<Props, undefined> {
+    render() {
+        return (
+            <h1>Hello from {this.props.compiler} and {this.props.framework}</h1>
+        )
+    }
+}
